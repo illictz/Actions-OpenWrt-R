61605adf0617f39e10769988ef4d8aafb1121827
FakeRaymond <62877862+FakeRaymond@users.noreply.github.com>
2023-02-15 15:21:02 +0000

    init

---
+++ diy-part1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
