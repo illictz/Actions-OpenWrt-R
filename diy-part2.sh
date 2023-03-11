1c42c17e08c132744f6e519cbb32f2dbe85a42b7
FakeRaymond <62877862+FakeRaymond@users.noreply.github.com>
2023-03-11 13:10:04 +0000

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.3/g' package/base-files/files/bin/config_generate
 
 # Add Geo Files
-mkdir -p ./files/usr/share/v2ray
-curl -fsSL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -o ./files/usr/share/v2ray/geoip.dat
-curl -fsSL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -o ./files/usr/share/v2ray/geosite.dat
-chmod -R 777 ./files/usr/share/v2ray
+# mkdir -p ./files/usr/share/v2ray
+# curl -fsSL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -o ./files/usr/share/v2ray/geoip.dat
+# curl -fsSL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -o ./files/usr/share/v2ray/geosite.dat
+# chmod -R 777 ./files/usr/share/v2ray
