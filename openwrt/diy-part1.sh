2c00aae03632aa0753f37cf04bd82c34a5f7d9f0
FakeRaymond <62877862+FakeRaymond@users.noreply.github.com>
2023-04-05 07:12:40 +0000

    update

---
+++ openwrt/diy-part1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i "/helloworld/d" "feeds.conf.default"
+sed -i "/passwall/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+# echo "src-git Passwall https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git Passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
+echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+# echo "src-git SmartdnsNew https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-new-version" >> feeds.conf.default
