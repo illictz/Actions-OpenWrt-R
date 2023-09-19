32708234d50ddf935775e7115de3a5cf25fadc96
FakeRaymond <62877862+FakeRaymond@users.noreply.github.com>
2023-09-19 14:26:11 +0000

    update

--- openwrt-passwall2/diy-part1.sh
+++ openwrt-passwall2/diy-part1.sh
@@ -6,7 +6,5 @@
 # Add a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
 sed -i "/passwall/d" "feeds.conf.default"
-# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
-echo "src-git Passwall https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
-# echo "src-git Passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
